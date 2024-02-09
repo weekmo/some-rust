6f69dc428519b558c48e3868afb9048db155153b
Mohammed <weekmo@aol.com>
2024-02-09 12:13:46 +0100

    second push

--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,14 @@ use regex::Regex;
 
 fn main() {
     secon_lesson();
-    let x1: u32 = 98_222;
-    let x2: u32 = 0x17FAE;
-    let x3: u32 = 0o277656;
-    let x4: u32 = 0b1_0111_1111_1010_1110;
-    let x5 = b'A';
-    print!("Dec: {}\nHex: {:#X}\nOct: {:#o}\nBin: {:#b}\nChar: {}\n", x1,x2,x3,x4,x5);
-
-    // addition
-    let sum = 5 + 10;
-
-    // subtraction
-    let difference = 95.5 - 4.3;
-
-    // multiplication
-    let product = 4 * 30;
-
-    // division
-    let quotient = 56.7 / 32.2;
-    let truncated = -5 / 3; // Results in -1
-
-    // remainder
-    let remainder = 43 % 5;
-
-    print!("Sum: {}\ndifference: {}\nproduct: {}\nquotient: {}\ntruncated: {}\nremainder: {}\n", sum,difference,product,quotient,truncated,remainder);
-
-    let c = 'z';
-    let z: char = 'Z'; // with explicit type annotation
-    let heart_eyed_cat = '😻';
-    let s = "Hello world";
-    
-    print!("c: {}\nz: {}\nheart_eyed_cat: {}\nString: {}\n",c,z,heart_eyed_cat, s);
-    let mut t = String::from("Hello you");
-    t.push_str(", world!"); // push_str() appends a literal to a String
-
-    println!("{}", t);
-
-    let a: [i32; 5] = [11, 21, 31, 41, 51];
-
-    for el in a {
-        println!("El: {}", el);
-    }
-
+    calc();
+    let s = String::from("Hello world");
+    let word = first_word(&s);
+    println!("{word}");
+    let hello = &s[0..5];
+    println!("{hello}");
+    let t = first_word("Hi Hello You");
+    println!("{t}");
 }
 
 /*
@@ -95,5 +61,59 @@ fn secon_lesson(){
     println!("Did our date match? {}", re.is_match("2014-01-01"));
 }
 
+fn calc(){
+    let x1: u32 = 98_222;
+    let x2: u32 = 0x17FAE;
+    let x3: u32 = 0o277656;
+    let x4: u32 = 0b1_0111_1111_1010_1110;
+    let x5 = b'A';
+    print!("Dec: {}\nHex: {:#X}\nOct: {:#o}\nBin: {:#b}\nChar: {}\n", x1,x2,x3,x4,x5);
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    print!("Sum: {}\ndifference: {}\nproduct: {}\nquotient: {}\ntruncated: {}\nremainder: {}\n", sum,difference,product,quotient,truncated,remainder);
+
+    let c = 'z';
+    let z: char = 'Z'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+    let s = "Hello world";
+    
+    print!("c: {}\nz: {}\nheart_eyed_cat: {}\nString: {}\n",c,z,heart_eyed_cat, s);
+    let mut t = String::from("Hello you");
+    t.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", t);
+
+    let a: [i32; 5] = [11, 21, 31, 41, 51];
+
+    for el in a {
+        println!("El: {}", el);
+    }
+}
+
+fn first_word(s: &str) -> &str {
+    let word_bytes = s.as_bytes();
+
+    for (i, &item) in word_bytes.iter().enumerate(){
+        if item == b' ' {
+            return  &s[..i];
+        }
+    }
+    &s[..]
+}
 // https://doc.rust-lang.org/book/ch04-03-slices.html
 // https://doc.rust-lang.org/cargo/guide/project-layout.html
